311fcfcea223fa80218ba2c8d3a18f8aca2ca5d3
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-02-17 19:38:23 +0200

    Add project files.

---
+++ Advanced3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Advanced3
+{
+    class Program
+    {
+        static void SerializeAndWrite()
+        {
+            User[] users = new User[5];
+            for (int i = 0; i < users.Length; i++)
+            {
+                users[i] = new User
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = $"Vasya{i}",
+                    LastName = $"Pupkin{i}",
+                    Age = 30 + i
+                };
+            }
+
+            using (var streamWriter = new StreamWriter("data.txt"))
+            {
+                streamWriter.Write(JsonSerializer.Serialize(users));
+            }
+        }
+
+        static void ReadAndDeserialize()
+        {
+            string input;
+            using (var streamReader = new StreamReader("data.txt"))
+            {
+                input = streamReader.ReadLine();
+            }
+
+            var users = JsonSerializer.Deserialize<IEnumerable<User>>(input);
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Advanced3/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Advanced3
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            //return JsonSerializer.Serialize(this);
+            return $"{Id} {FirstName} {LastName} {Age}";
+        }
+    }
+}
